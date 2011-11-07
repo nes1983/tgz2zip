5785f2b3e8ca918ee4b483110d0968d3d4017c9d
Niko Schwarz <niko.schwarz@gmail.com>
2011-11-07 16:30:47 +0100

    Made the test work

--- test.sh
+++ test.sh
@@ -1,4 +1,27 @@
 rm test.zip 2> /dev/null
-tar -cjf test.tar.bzip2 test
-sh tgz2zip.sh test.tar.bzip2 test.zip
+trap "rm -rf 'test.zip'" EXIT INT HUP TERM
+rm -rf test 2> /dev/null
+trap "rm -rf 'test'" EXIT INT HUP TERM
 rm test.tar.bzip2 2> /dev/null
+trap "rm -rf 'test.tar.bzip2'" EXIT INT HUP TERM
+
+
+mkdir test
+cd test
+touch hi
+touch hi2
+cd ..
+
+tar -cjf test.tar.bzip2 test
+
+sh ./tgz2zip test.tar.bzip2
+
+out=`unzip -l test.zip` 
+if [ "8" == `echo "$out" | wc -l` ] ; then
+	echo "TEST PASSED"
+	exit 0
+else
+	echo "TEST FAILED"
+	echo "$out"
+	exit -1
+fi
