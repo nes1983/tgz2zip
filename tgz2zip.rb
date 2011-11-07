23cdc82435c8af8a332f535b02233716670d5564
Niko Schwarz <niko.schwarz@gmail.com>
2011-11-07 17:18:11 +0100

    Moved the test over into the brew file.
    You just can't conveniently find your stuff 
    in brew tests.

--- tgz2zip.rb
+++ tgz2zip.rb
@@ -2,17 +2,44 @@ require 'formula'
 require 'install'
 
 class Tgz2zip < Formula
-  url 'https://github.com/nes1983/tgz2zip/zipball/master'
-  homepage 'http://github.com/nes1983/tgz2zip/'
-  #md5 '829a2db4700c4c5ae549b0133a1d0d48'
-  version '0.1'
-  depends_on 'ronn' => :ruby
-  
-  def install
-      install_tgz2zip(prefix)
-  end
+	url 'https://github.com/nes1983/tgz2zip/zipball/master'
+	homepage 'http://github.com/nes1983/tgz2zip/'
+	#md5 '829a2db4700c4c5ae549b0133a1d0d48'
+	version '0.1'
+	depends_on 'ronn' => :ruby
 
-  def test
-    system("./test.sh")
-  end
+	def install
+			install_tgz2zip(prefix)
+	end
+
+	def test
+			`cd /tmp`
+			puts `rm test.zip 2> /dev/null
+			trap "rm -rf 'test.zip'" EXIT INT HUP TERM
+			rm -rf test 2> /dev/null
+			trap "rm -rf 'test'" EXIT INT HUP TERM
+			rm test.tar.bzip2 2> /dev/null
+			trap "rm -rf 'test.tar.bzip2'" EXIT INT HUP TERM
+
+
+			mkdir test
+			cd test
+			touch hi
+			touch hi2
+			cd ..
+
+			tar -cjf test.tar.bzip2 test
+
+			tgz2zip test.tar.bzip2
+
+			out=\`unzip -l test.zip\`
+			if [ "8" == \`echo "$out" | wc -l\` ] ; then
+					echo "TEST PASSED"
+					exit 0
+			else
+					echo "TEST FAILED"
+					echo "$out"
+					exit -1
+			fi`
+	end
 end
